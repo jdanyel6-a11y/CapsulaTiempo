783b9de00d4b91f15f1b9ceb9d9a54ce911a1561
macbook <macbook@192.168.1.14>
2026-03-08 22:20:17 +0100

    Versión 1.0: Lanzamiento inicial

---
+++ CapsulaTiempo/src/Mensaje.java
@@ -0,0 +1,10 @@
+
+public class Mensaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje para el año 2026: ¡Git funciona!");
+
+	}
+
+}
